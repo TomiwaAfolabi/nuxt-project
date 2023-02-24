32228cf65d13eeb4e81a9347f984731a804697ea
TomiwaAfolabi <tomiwasanto@gmail.com>
2023-02-24 23:42:48 +0100

    improved on sidebar design for right and left side

---
+++ Desktop/Nuxt/twitterClone/components/Sidebar/Right/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col">
+    <SidebarRightTab title="What's Happening" />
+    <SidebarRightTab title="Who to Follow" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ Desktop/Nuxt/twitterClone/components/Sidebar/Right/Tab.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="m-2 bg-gray-50 rounded-2xl dark:bg-dim-700 overflow-hidden"
+    :class="twitterBorderColor"
+  >
+    <h1
+      class="p-3 text-gray-900 text-xl border-b font-extrabold dark:text-white"
+    >
+      {{ props.title }}
+    </h1>
+
+    <slot></slot>
+    <div
+      class="p-3 text-sm text-blue-400 cursor-pointer hover:bg-gray-100 dark:hover:bg-dim-300"
+    >
+      Show more
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { twitterBorderColor } = useTailwindConfig();
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
